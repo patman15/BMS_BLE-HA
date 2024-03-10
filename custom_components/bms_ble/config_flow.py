3979e7a474d82e7063b173a03fed0dd88e95f7ef
patman15 <14628713+patman15@users.noreply.github.com>
2024-03-10 19:31:24 +0100

    added user config flow

--- custom_components/bms_ble/config_flow.py
+++ custom_components/bms_ble/config_flow.py
@@ -4,6 +4,11 @@ from habluetooth import BluetoothServiceInfoBleak
 from homeassistant import config_entries
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.helpers.device_registry import format_mac
+from homeassistant.helpers.selector import (
+    SelectSelector, SelectSelectorConfig)
+from homeassistant.components.bluetooth import async_discovered_service_info
+from homeassistant.const import CONF_ADDRESS
+import voluptuous as vol
 from typing import Any
 
 from .const import DOMAIN
@@ -21,6 +26,8 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     def __init__(self) -> None:
         """Initialize the config flow."""
         self._discovery_info: BluetoothServiceInfoBleak | None = None
+        self._discovered_device = None
+        self._discovered_devices: dict[str, BluetoothServiceInfoBleak] = {}
 
     async def async_step_bluetooth(
         self, discovery_info: BluetoothServiceInfoBleak
@@ -50,3 +57,44 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         self._set_confirm_only()
         placeholders = {"name": title}
         return self.async_show_form(step_id="bluetooth_confirm", description_placeholders=placeholders)
+
+    async def async_step_user(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Handle the user step to pick discovered device."""
+        if user_input is not None:
+            address = user_input[CONF_ADDRESS]
+            await self.async_set_unique_id(address, raise_on_progress=False)
+            self._abort_if_unique_id_configured()
+            discovery = self._discovered_devices[address]
+
+            self.context["title_placeholders"] = {"name": discovery.name}
+
+            self._discovered_device = discovery.device
+
+            return self.async_create_entry(title=discovery.name, data={})
+
+        current_addresses = self._async_current_ids()
+        for discovery_info in async_discovered_service_info(self.hass, False):
+            address = discovery_info.address
+            if address in current_addresses or address in self._discovered_devices:
+                continue
+            # TODO: for more general approach a supported() method is required
+            if not discovery_info.name.startswith("SmartBat-"):
+                continue
+
+            # if device.supported(discovery_info):
+            self._discovered_devices[address] = discovery_info
+
+        if not self._discovered_devices:
+            return self.async_abort(reason="no_devices_found")
+
+        titles = []
+        for (address, discovery) in self._discovered_devices.items():
+            titles.append({"value": address, "label": discovery.name})
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({vol.Required(CONF_ADDRESS): SelectSelector(
+                SelectSelectorConfig(options=titles))}),
+        )
