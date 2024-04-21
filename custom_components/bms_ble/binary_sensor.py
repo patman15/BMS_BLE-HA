8bc84815fbd9c84db871b2f611187b86ee14db3a
patman15 <14628713+patman15@users.noreply.github.com>
2024-04-21 17:07:39 +0200

    remove future import

--- custom_components/bms_ble/binary_sensor.py
+++ custom_components/bms_ble/binary_sensor.py
@@ -1,7 +1,5 @@
 """Support for BMS_BLE binary sensors."""
 
-from __future__ import annotations
-
 from homeassistant.components.binary_sensor import (
     BinarySensorDeviceClass,
     BinarySensorEntity,
