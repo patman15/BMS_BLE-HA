c31d482cb3872faa8d0a73b248683b3e0d83a1b5
patman15 <14628713+patman15@users.noreply.github.com>
2024-05-01 20:14:40 +0200

    made runtime integer
    Since runtime is in seconds, type float does not deliver any value

--- custom_components/bms_ble/plugins/basebms.py
+++ custom_components/bms_ble/plugins/basebms.py
@@ -88,7 +88,7 @@ class BaseBMS(metaclass=ABCMeta):
         # calculate runtime from current and cycle charge
         if can_calc(ATTR_RUNTIME, frozenset({ATTR_CURRENT, ATTR_CYCLE_CHRG})):
             if data[ATTR_CURRENT] > 0:
-                data[ATTR_RUNTIME] = (
+                data[ATTR_RUNTIME] = int(
                     data[ATTR_CYCLE_CHRG] / data[ATTR_CURRENT] * _HRS_TO_SECS
                 )
 
