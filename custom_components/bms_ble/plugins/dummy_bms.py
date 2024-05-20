e09097c4262a0d9cd41e4cef4e7d8207ed0171ea
patman15 <14628713+patman15@users.noreply.github.com>
2024-05-20 18:13:52 +0200

    added dummy_bms for tests

---
+++ custom_components/bms_ble/plugins/dummy_bms.py
@@ -0,0 +1,54 @@
+"""Module to support Dummy BMS."""
+
+import logging
+from typing import Any
+
+from bleak.backends.device import BLEDevice
+
+from ..const import (
+    ATTR_BATTERY_CHARGING,
+    # ATTR_BATTERY_LEVEL,
+    ATTR_CURRENT,
+    # ATTR_CYCLE_CAP,
+    # ATTR_CYCLE_CHRG,
+    # ATTR_CYCLES,
+    ATTR_POWER,
+    # ATTR_RUNTIME,
+    # ATTR_TEMPERATURE,
+    ATTR_VOLTAGE,
+)
+from .basebms import BaseBMS
+
+LOGGER = logging.getLogger(__name__)
+
+
+class BMS(BaseBMS):
+    """Dummy battery class implementation."""
+
+    def __init__(self, ble_device: BLEDevice, reconnect: bool = False) -> None:
+        """Initialize BMS."""
+        LOGGER.debug("%s init(), BT address: %s", self.device_id(), ble_device.address)
+
+    @staticmethod
+    def matcher_dict_list() -> list[dict[str, Any]]:
+        """Provide BluetoothMatcher definition."""
+        return [{"local_name": "dummy", "connectable": True}]
+
+    @staticmethod
+    def device_info() -> dict[str, str]:
+        """Return device information for the battery management system."""
+        return {"manufacturer": "Dummy Manufacturer", "model": "dummy model"}
+
+    async def disconnect(self) -> None:
+        """Disconnect connection to BMS if active."""
+
+    async def async_update(self) -> dict[str, int | float | bool]:
+        """Update battery status information."""
+        data = {
+            ATTR_VOLTAGE: 12,
+            ATTR_CURRENT: 1.5,
+        }  # set fixed values for dummy battery
+        self.calc_values(
+            data, {ATTR_POWER, ATTR_BATTERY_CHARGING}
+        )  # calculate further values from previously set ones
+        return data
