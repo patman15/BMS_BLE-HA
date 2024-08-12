65f15cc9b2fba7e65ce22d5a530d9decc17a8b98
patman15 <14628713+patman15@users.noreply.github.com>
2024-08-12 21:22:58 +0200

    Create test_const.py

---
+++ tests/test_const.py
@@ -0,0 +1,10 @@
+"""Test the BLE Battery Management System integration constants definition."""
+
+from custom_components.bms_ble.const import UPDATE_INTERVAL, BMS_TYPES
+
+
+async def test_update_interval() -> None:
+    """Test general constants are not altered for debugging."""
+
+    assert UPDATE_INTERVAL == 30  # ensure that update interval is 30 seconds
+    assert len(BMS_TYPES) == 5  # check number of BMS types
