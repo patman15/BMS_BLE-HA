02c0cac54d7aed8607294970caddd9f2be37ed23
patman15 <14628713+patman15@users.noreply.github.com>
2024-10-24 18:44:47 +0200

    deduplicated main BMS functions
    centralized __init__, _on_disconnect, _connect, _disconnect

--- custom_components/bms_ble/plugins/dummy_bms.py
+++ custom_components/bms_ble/plugins/dummy_bms.py
@@ -31,6 +31,7 @@ class BMS(BaseBMS):
 
     def __init__(self, ble_device: BLEDevice, reconnect: bool = False) -> None:
         """Initialize BMS."""
+        super().__init__(LOGGER, self._notification_handler, ble_device, reconnect)
         LOGGER.debug("%s init(), BT address: %s", self.device_id(), ble_device.address)
         super().__init__(LOGGER, self._notification_handler, ble_device, reconnect)
 
