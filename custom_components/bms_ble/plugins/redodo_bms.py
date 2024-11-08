13531c4c3ecde2eeeb9b031d93a731f8709e23b8
patman15 <14628713+patman15@users.noreply.github.com>
2024-11-08 10:59:28 +0100

    initial version
    missing SoC, remaining capacity, cycles, temperature

---
+++ custom_components/bms_ble/plugins/redodo_bms.py
@@ -0,0 +1,152 @@
+"""Module to support Dummy BMS."""
+
+import asyncio
+import logging
+from typing import Any, Final, Callable
+
+from bleak.backends.device import BLEDevice
+from bleak.uuids import normalize_uuid_str
+
+from custom_components.bms_ble.const import (
+    ATTR_BATTERY_CHARGING,
+    # ATTR_BATTERY_LEVEL,
+    ATTR_CURRENT,
+    # ATTR_CYCLE_CAP,
+    # ATTR_CYCLE_CHRG,
+    # ATTR_CYCLES,
+    ATTR_DELTA_VOLTAGE,
+    ATTR_POWER,
+    # ATTR_RUNTIME,
+    # ATTR_TEMPERATURE,
+    ATTR_VOLTAGE,
+    KEY_CELL_VOLTAGE,
+)
+
+from .basebms import BaseBMS, BMSsample
+
+LOGGER = logging.getLogger(__name__)
+BAT_TIMEOUT = 10
+
+
+class BMS(BaseBMS):
+    """Dummy battery class implementation."""
+
+    CRC_POS: Final = -1  # last byte
+    HEAD_LEN: Final = 3
+    MAX_CELLS: Final = 16
+
+    def __init__(self, ble_device: BLEDevice, reconnect: bool = False) -> None:
+        """Initialize BMS."""
+        LOGGER.debug("%s init(), BT address: %s", self.device_id(), ble_device.address)
+        super().__init__(LOGGER, self._notification_handler, ble_device, reconnect)
+
+        self._data: bytearray = bytearray()
+        self._FIELDS: Final[
+            list[tuple[str, int, int, bool, Callable[[int], int | float]]]
+        ] = [
+            (ATTR_VOLTAGE, 12, 2, False, lambda x: float(x / 1000)),
+            (ATTR_CURRENT, 48, 4, True, lambda x: float(x / 1000)),
+            # (ATTR_TEMPERATURE, 0x09, 4, 2, False, lambda x: x),
+            # (ATTR_BATTERY_LEVEL, 0x0A, 4, 1, False, lambda x: x),
+            # (KEY_DESIGN_CAP, 0x15, 4, 2, False, lambda x: x),
+            # (ATTR_CYCLES, 0x15, 6, 2, False, lambda x: x),
+            # (ATTR_RUNTIME, 0x0C, 14, 2, False, lambda x: float(x * _HRS_TO_SECS / 100)),
+        ]
+
+    @staticmethod
+    def matcher_dict_list() -> list[dict[str, Any]]:
+        """Provide BluetoothMatcher definition."""
+        return [
+            {
+                "service_uuid": BMS.uuid_services()[0],
+                "manufacturer_id": 0x585A,
+                "connectable": True,
+            }
+        ]
+
+    @staticmethod
+    def device_info() -> dict[str, str]:
+        """Return device information for the battery management system."""
+        return {"manufacturer": "Redodo", "model": "Bluetooth battery"}
+
+    @staticmethod
+    def uuid_services() -> list[str]:
+        """Return list of 128-bit UUIDs of services required by BMS."""
+        return [normalize_uuid_str("ffe0")]  # change service UUID here!
+
+    @staticmethod
+    def uuid_rx() -> str:
+        """Return 16-bit UUID of characteristic that provides notification/read property."""
+        return "ffe1"
+
+    @staticmethod
+    def uuid_tx() -> str:
+        """Return 16-bit UUID of characteristic that provides write property."""
+        return "ffe2"
+
+    @staticmethod
+    def _calc_values() -> set[str]:
+        return {
+            ATTR_BATTERY_CHARGING,
+            ATTR_DELTA_VOLTAGE,
+            ATTR_POWER,
+        }  # calculate further values from BMS provided set ones
+
+    def _notification_handler(self, _sender, data: bytearray) -> None:
+        """Handle the RX characteristics notify event (new data arrives)."""
+        LOGGER.debug("%s: Received BLE data: %s", self.name, data.hex(" "))
+
+        if len(data) < 3 or not data.startswith(b"\x00\x00"):
+            LOGGER.debug("%s: incorrect SOF.")
+            return
+
+        if len(data) != data[2] + self.HEAD_LEN + 1:  # add header length and CRC
+            LOGGER.debug("(%s) incorrect frame length (%i)", self.name, len(data))
+            return
+
+        crc = self._crc(data[: self.CRC_POS])
+        if crc != data[self.CRC_POS]:
+            LOGGER.debug(
+                "(%s) Rx data CRC is invalid: 0x%x != 0x%x",
+                self.name,
+                data[len(data) + self.CRC_POS],
+                crc,
+            )
+            return
+
+        self._data = data
+        self._data_event.set()
+
+    def _crc(self, frame: bytes) -> int:
+        """Calculate frame CRC."""
+        return sum(frame) & 0xFF
+
+    def _cell_voltages(self, data: bytearray, cells: int) -> dict[str, float]:
+        """Return cell voltages from status message."""
+        return {
+            f"{KEY_CELL_VOLTAGE}{idx}": int.from_bytes(
+                data[16 + 2 * idx : 16 + 2 * idx + 2],
+                byteorder="little",
+                signed=False,
+            )
+            / 1000
+            for idx in range(cells)
+            if int.from_bytes(data[16 + 2 * idx : 16 + 2 * idx + 2], byteorder="little")
+        }
+
+    async def _async_update(self) -> BMSsample:
+        """Update battery status information."""
+        LOGGER.debug("(%s) replace with command to UUID %s", self.name, BMS.uuid_tx())
+        await self._client.write_gatt_char(
+            BMS.uuid_tx(), data=b"\x00\x00\x04\x01\x13\x55\xaa\x17"
+        )
+        await asyncio.wait_for(self._wait_event(), timeout=BAT_TIMEOUT)
+
+        return {
+            key: func(
+                int.from_bytes(
+                    self._data[idx : idx + size], byteorder="little", signed=sign
+                )
+            )
+            for key, idx, size, sign, func in self._FIELDS
+        } | self._cell_voltages(self._data, self.MAX_CELLS)
---
+++ tests/test_redodo_bms.py
@@ -0,0 +1,200 @@
+"""Test the E&J technology BMS implementation."""
+
+import pytest
+from collections.abc import Buffer
+from uuid import UUID
+
+from bleak.backends.characteristic import BleakGATTCharacteristic
+from bleak.exc import BleakError
+from bleak.uuids import normalize_uuid_str
+from custom_components.bms_ble.plugins.redodo_bms import BMS
+
+from .bluetooth import generate_ble_device
+from .conftest import MockBleakClient
+
+
+class MockRedodoBleakClient(MockBleakClient):
+    """Emulate a Redodo BMS BleakClient."""
+
+    def _response(
+        self, char_specifier: BleakGATTCharacteristic | int | str | UUID, data: Buffer
+    ) -> bytearray:
+        if isinstance(char_specifier, str) and normalize_uuid_str(
+            char_specifier
+        ) != normalize_uuid_str("ffe2"):
+            return bytearray()
+        cmd: int = bytearray(data)[4]
+        if cmd == 0x13:
+            return bytearray(
+                b"\x00\x00\x65\x01\x93\x55\xaa\x00\x46\x66\x00\x00\xbc\x67\x00\x00\xf5\x0c\xf7\x0c"
+                b"\xfc\x0c\xfb\x0c\xf8\x0c\xf2\x0c\xfa\x0c\xf5\x0c\x00\x00\x00\x00\x00\x00\x00\x00"
+                b"\x00\x00\x00\x00\x00\x00\x00\x00\x65\xfa\xff\xff\x17\x00\x16\x00\x17\x00\x00\x00"
+                b"\x00\x00\xe9\x1a\x04\x29\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+                b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x41\x00\x64\x00\x00\x00\x03\x00\x00\x00"
+                b"\x5f\x01\x00\x00\xbc"
+            )  # TODO: put numbers
+        return bytearray()
+
+    async def write_gatt_char(
+        self,
+        char_specifier: BleakGATTCharacteristic | int | str | UUID,
+        data: Buffer,
+        response: bool = None,  # type: ignore[implicit-optional] # noqa: RUF013 # same as upstream
+    ) -> None:
+        """Issue write command to GATT."""
+        await super().write_gatt_char(char_specifier, data, response)
+        assert self._notify_callback is not None
+        self._notify_callback(
+            "MockRedodoBleakClient", self._response(char_specifier, data)
+        )
+
+
+# class MockWrongCRCBleakClient(MockRedodoBleakClient):
+#     """Emulate a E&J technology BMS BleakClient that replies with wrong CRC"""
+
+#     def _response(
+#         self, char_specifier: BleakGATTCharacteristic | int | str | UUID, data: Buffer
+#     ) -> bytearray:
+#         if isinstance(char_specifier, str) and normalize_uuid_str(
+#             char_specifier
+#         ) != normalize_uuid_str("fff3"):
+#             return bytearray()
+#         cmd: int = int(bytearray(data)[5])
+#         if cmd == 0x60:
+#             return bytearray(
+#                 b"\x12\x12\x3A\x05\x03\x60\x00\x0A\x02\x13\x00\x00\x71\xC5\x45\x8E\x3D\x00\x01\xCE"
+#                 b"\x02\x22\x0D\x0A\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+#             )  # wrong CRC [0x01CE != 0x02CD] in line 1
+#         if cmd == 0x61:
+#             return bytearray(
+#                 b"\x12\x12\x3A\x05\x03\x61\x00\x0C\x00\x12\x00\x12\x6D\x60\x0B\x7E\x8F\xDB\x18\x20"
+#                 b"\x04\x22\x02\x91\x0D\x0A\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+#             )  # wrong CRC [0x02 != 0x03] in line 2
+#         if cmd == 0x62:
+#             return bytearray(
+#                 b"\x12\x13\x3A\x05\x03\x62\x00\x1D\x0E\x0E\xD7\x0E\xD6\x0E\xD6\x0E\xD5\x0E\xD5\x0E"
+#                 b"\x12\x23\xD6\x0E\xD1\x0E\xD2\x0E\xD5\x0E\xD6\x0E\xD4\x0E\xD8\x0E\xD7\x0E\xDB\x0E"
+#                 b"\x03\x33\x08\x0D\x0A\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+#             )  # wrong CRC [0x0E != 0x0D] in line 2
+#         return bytearray()
+
+
+# class MockInvalidBleakClient(MockRedodoBleakClient):
+#     """Emulate a E&J technology BMS BleakClient replying garbage."""
+
+#     def _response(
+#         self, char_specifier: BleakGATTCharacteristic | int | str | UUID, data: Buffer
+#     ) -> bytearray:
+#         if isinstance(char_specifier, str) and normalize_uuid_str(
+#             char_specifier
+#         ) != normalize_uuid_str("6e400002-b5a3-f393-e0a9-e50e24dcca9e"):
+#             return bytearray()
+#         cmd: int = int(bytearray(data)[3:5], 16)
+#         if cmd == 0x02:
+#             return bytearray(
+#                 b"X0082310080000101C00000880F540F3C0F510FD70F310F2C0F340F3A0FED0FED0000000000000000000000000000000248424242F0000000000000000001ABX"
+#             )  # correct message, wrong SOI
+#         if cmd == 0x10:
+#             return bytearray(
+#                 b":009031001D0000001400080016F4~"
+#             )  # incorrect message, wrong length (1E != 1D)
+#         return bytearray()
+
+#     async def disconnect(self) -> bool:
+#         """Mock disconnect to raise BleakError."""
+#         raise BleakError
+
+
+async def test_update(monkeypatch, reconnect_fixture) -> None:
+    """Test Redodo technology BMS data update."""
+
+    monkeypatch.setattr(
+        "custom_components.bms_ble.plugins.basebms.BleakClient",
+        MockRedodoBleakClient,
+    )
+
+    bms = BMS(
+        generate_ble_device("cc:cc:cc:cc:cc:cc", "MockBLEDevice", None, -73),
+        reconnect_fixture,
+    )
+
+    result = await bms.async_update()
+
+    assert result == {
+        "voltage": 26.556,
+        "current": -1.435,
+        "cell#0": 3.317,
+        "cell#1": 3.319,
+        "cell#2": 3.324,
+        "cell#3": 3.323,
+        "cell#4": 3.320,
+        "cell#5": 3.314,
+        "cell#6": 3.322,
+        "cell#7": 3.317,
+        "delta_voltage": 0.01,
+        "power": -38.108,
+        "battery_charging": False,
+    }
+
+    # query again to check already connected state
+    result = await bms.async_update()
+    assert bms._client.is_connected is not reconnect_fixture  # noqa: SLF001
+
+    await bms.disconnect()
+
+
+@pytest.fixture(name="wrong_response", params=[
+        b"x009031001E0000001400080016F4~", # wrong SOI
+        b":009031001E0000001400080016F4x", # wrong EOI
+        b":009031001D0000001400080016F4~", # wrong length
+    ])
+def response(request):
+    """Return all possible BMS variants."""
+    return request.param
+
+# async def test_invalid_response(monkeypatch, wrong_response) -> None:
+#     """Test data up date with BMS returning invalid data."""
+
+#     monkeypatch.setattr(
+#         "custom_components.bms_ble.plugins.redodo_bms.BAT_TIMEOUT",
+#         0.1,
+#     )
+
+#     monkeypatch.setattr(
+#         "tests.test_redodo_bms.MockRedodoBleakClient._response",
+#         lambda _s,_c_,d: wrong_response,
+#     )
+
+#     monkeypatch.setattr(
+#         "custom_components.bms_ble.plugins.basebms.BleakClient",
+#         MockEJBleakClient,
+#     )
+
+#     bms = BMS(generate_ble_device("cc:cc:cc:cc:cc:cc", "MockBLEDevice", None, -73))
+
+#     result = {}
+#     with pytest.raises(TimeoutError):
+#         result = await bms.async_update()
+
+#     assert not result
+#     await bms.disconnect()
+
+
+# async def test_wrong_crc(monkeypatch, dev_name) -> None:
+#     """Test data update with BMS returning invalid data."""
+
+#     monkeypatch.setattr(
+#         "custom_components.bms_ble.plugins.dpwrcore_bms.BAT_TIMEOUT",
+#         0.1,
+#     )
+
+#     monkeypatch.setattr(
+#         "custom_components.bms_ble.plugins.basebms.BleakClient",
+#         MockWrongCRCBleakClient,
+#     )
+
+#     bms = BMS(generate_ble_device("cc:cc:cc:cc:cc:cc", dev_name, None, -73))
+
+#     assert await bms.async_update() == {}
+
+#     await bms.disconnect()
