6d3c8819be5abb8bf404823023abdc8d51b064a3
patman15 <14628713+patman15@users.noreply.github.com>
2024-12-14 21:24:46 +0100

    detailed BT matcher for JBD

--- custom_components/bms_ble/plugins/jbd_bms.py
+++ custom_components/bms_ble/plugins/jbd_bms.py
@@ -58,6 +58,17 @@ class BMS(BaseBMS):
         """Provide BluetoothMatcher definition."""
         return [
             {
+                "local_name": "SP0?S*",
+                "service_uuid": BMS.uuid_services()[0],
+                "connectable": True,
+            },
+            {
+                "local_name": "SP1?S*",
+                "service_uuid": BMS.uuid_services()[0],
+                "connectable": True,
+            },
+            {
+                "local_name": "SP2?S*",
                 "service_uuid": BMS.uuid_services()[0],
                 "connectable": True,
             },
