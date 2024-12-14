95662a8a96b52d9b9159422d8113c634c752ee69
patman15 <14628713+patman15@users.noreply.github.com>
2024-12-14 12:00:20 +0100

    fix cycle charge

--- custom_components/bms_ble/plugins/tdt_bms.py
+++ custom_components/bms_ble/plugins/tdt_bms.py
@@ -12,6 +12,7 @@ from custom_components.bms_ble.const import (
     ATTR_BATTERY_LEVEL,
     ATTR_CURRENT,
     ATTR_CYCLE_CAP,
+    ATTR_CYCLE_CHRG,
     ATTR_CYCLES,
     ATTR_DELTA_VOLTAGE,
     ATTR_POWER,
@@ -48,7 +49,7 @@ class BMS(BaseBMS):
             False,
             lambda x: float((x & 0x3FFF) / 10 * (-1 if x >> 15 else 1)),
         ),
-        (ATTR_CYCLE_CAP, 0x8C, 58, 2, False, lambda x: float(x / 10)),
+        (ATTR_CYCLE_CHRG, 0x8C, 58, 2, False, lambda x: float(x / 10)),
         (ATTR_BATTERY_LEVEL, 0x8C, 67, 1, False, lambda x: x),
         (ATTR_CYCLES, 0x8C, 62, 2, False, lambda x: x),
         (KEY_CELL_COUNT, 0x8C, 8, 1, False, lambda x: x),
@@ -98,9 +99,10 @@ class BMS(BaseBMS):
     @staticmethod
     def _calc_values() -> set[str]:
         return {
-            ATTR_POWER,
             ATTR_BATTERY_CHARGING,
             ATTR_CYCLE_CAP,
+            ATTR_DELTA_VOLTAGE,
+            ATTR_POWER,
             ATTR_RUNTIME,
         }  # calculate further values from BMS provided set ones
 
@@ -152,13 +154,10 @@ class BMS(BaseBMS):
     def _cmd(cmd: int, data: bytearray = bytearray()) -> bytearray:
         """Assemble a TDT BMS command."""
         assert cmd in (0x8C, 0x8D, 0x92)  # allow only read commands
-        frame = bytearray(
-            [BMS._HEAD, 0x0, 0x1, 0x3, 0x0, cmd, 0x0, 0x0]
-        )  # fixed version
-        # frame += len(data).to_bytes(2, "big", signed=False) + data
+        frame = bytearray([BMS._HEAD, 0x0, 0x1, 0x3, 0x0, cmd])  # fixed version
+        frame += len(data).to_bytes(2, "big", signed=False) + data
         frame += bytearray(int.to_bytes(crc_modbus(frame), 2, byteorder="little"))
         frame += bytearray([BMS._TAIL])
-        LOGGER.debug("TX cmd: %s", frame.hex(" "))
         return frame
 
     @staticmethod
@@ -172,21 +171,17 @@ class BMS(BaseBMS):
             for key, cmd, idx, size, sign, func in BMS._FIELDS
         }
 
-    # @staticmethod
-    # def _cell_voltages(data: bytearray, offset: int = 0) -> dict[str, float]:
-    #     return {
-    #         f"{KEY_CELL_VOLTAGE}{idx+offset}": float(
-    #             int.from_bytes(
-    #                 data[
-    #                     BMS._CELLS_POS + 1 + idx * 2 : BMS._CELLS_POS + 1 + idx * 2 + 2
-    #                 ],
-    #                 byteorder="big",
-    #                 signed=False,
-    #             )
-    #         )
-    #         / 1000
-    #         for idx in range(data[BMS._CELLS_POS])
-    #     }
+    @staticmethod
+    def _cell_voltages(data: bytearray) -> dict[str, float]:
+        return {
+            f"{KEY_CELL_VOLTAGE}{idx}": float(
+                int.from_bytes(
+                    data[9 + idx * 2 : 9 + idx * 2 + 2], byteorder="big", signed=False
+                )
+            )
+            / 1000
+            for idx in range(data[8])
+        }
 
     async def _async_update(self) -> BMSsample:
         """Update battery status information."""
@@ -198,25 +193,9 @@ class BMS(BaseBMS):
             if cmd not in self._data_final:
                 return {}
 
-        result = BMS._decode_data(self._data_final)
-
-        # total_cells: int = 0
-        # for pack in range(int(result.get(KEY_PACK_COUNT, 0) + 1)):
-        #     await self._client.write_gatt_char(
-        #         BMS.uuid_tx(), data=BMS._cmd(0x61, data=bytearray([pack]))
-        #     )
-        #     await asyncio.wait_for(self._wait_event(), timeout=BAT_TIMEOUT)
-        #     pack_cells: dict[str, float] = BMS._cell_voltages(
-        #         self._data_final[0x61], total_cells
-        #     )
-        #     result |= pack_cells
-        #     result |= {
-        #         ATTR_DELTA_VOLTAGE: max(
-        #             float(result.get(ATTR_DELTA_VOLTAGE, 0)),
-        #             round(max(pack_cells.values()) - min(pack_cells.values()), 3),
-        #         )
-        #     }
-        #     total_cells += self._data_final[0x61][BMS._CELLS_POS]
+        result = BMS._decode_data(self._data_final) | BMS._cell_voltages(
+            self._data_final[0x8C]
+        )
 
         self._data_final.clear()
 
