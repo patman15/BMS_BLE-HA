6c3b1bd04fe166dd743190147f861398b427575e
patman15 <14628713+patman15@users.noreply.github.com>
2024-12-18 06:03:10 +0100

    switched to single machine data

--- custom_components/bms_ble/plugins/seplos_v2_bms.py
+++ custom_components/bms_ble/plugins/seplos_v2_bms.py
@@ -23,6 +23,7 @@ from custom_components.bms_ble.const import (
     KEY_CELL_VOLTAGE,
     KEY_PACK_COUNT,
     KEY_TEMP_SENS,
+    KEY_TEMP_VALUE,
 )
 
 from .basebms import BaseBMS, BMSsample, crc_xmodem
@@ -40,23 +41,17 @@ class BMS(BaseBMS):
     _RSP_VER: Final[int] = 0x14
     _MIN_LEN: Final[int] = 10
     _MAX_SUBS: Final[int] = 0xF
+    _CELL_POS: Final[int] = 9
     _FIELDS: Final[
         list[tuple[str, int, int, int, bool, Callable[[int], int | float]]]
     ] = [
-        (ATTR_VOLTAGE, 0x62, 25, 2, False, lambda x: float(x / 100)),
-        (ATTR_CURRENT, 0x62, 23, 2, True, lambda x: float(x / 10)),
-        (ATTR_CYCLE_CHRG, 0x62, 27, 2, False, lambda x: float(x / 10)),
-        (ATTR_CYCLES, 0x62, 36, 2, False, lambda x: x),
-        (ATTR_BATTERY_LEVEL, 0x62, 32, 2, False, lambda x: float(x / 10)),
-        (ATTR_TEMPERATURE, 0x62, 21, 2, True, lambda x: (x - 2731.5) / 10),
-        (KEY_CELL_COUNT, 0x62, 9, 1, False, lambda x: x),
-        (KEY_PACK_COUNT, 0x51, 42, 1, False, lambda x: min(x, BMS._MAX_SUBS)),
-        (KEY_TEMP_SENS, 0x62, 14, 1, False, lambda x: x),
+        (ATTR_VOLTAGE, 0x61, 2, 2, False, lambda x: float(x / 100)),
+        (ATTR_CURRENT, 0x61, 0, 2, True, lambda x: float(x / 10)),
+        (ATTR_CYCLE_CHRG, 0x61, 4, 2, False, lambda x: float(x / 10)),
+        (ATTR_CYCLES, 0x61, 13, 2, False, lambda x: x),
+        (ATTR_BATTERY_LEVEL, 0x61, 9, 2, False, lambda x: float(x / 10)),
     ]  # Protocol Seplos V2 (parallel data 0x62, device manufacturer info 0x51)
-    _CMDS: Final[list[tuple[int, bytes]]] = [
-        *list({(field[1], b"") for field in _FIELDS}),
-        (0x61, b"\x00"),
-    ]
+    _CMDS: Final[list[tuple[int, bytes]]] = [(0x51, b""), (0x61, b"\x00")]
 
     def __init__(self, ble_device: BLEDevice, reconnect: bool = False) -> None:
         """Initialize BMS."""
@@ -105,6 +100,7 @@ class BMS(BaseBMS):
             ATTR_DELTA_VOLTAGE,
             ATTR_POWER,
             ATTR_RUNTIME,
+            ATTR_TEMPERATURE,
         }  # calculate further values from BMS provided set ones
 
     def _notification_handler(self, _sender, data: bytearray) -> None:
@@ -177,14 +173,36 @@ class BMS(BaseBMS):
         return frame
 
     @staticmethod
-    def _decode_data(data: dict[int, bytearray]) -> dict[str, int | float]:
+    def _decode_data(data: dict[int, bytearray], offs: int) -> dict[str, int | float]:
         return {
             key: func(
                 int.from_bytes(
-                    data[cmd][idx : idx + size], byteorder="big", signed=sign
+                    data[cmd][idx + offs : idx + offs + size],
+                    byteorder="big",
+                    signed=sign,
                 )
             )
             for key, cmd, idx, size, sign, func in BMS._FIELDS
+        }
+
+    @staticmethod
+    def _temp_sensors(data: bytearray, sensors: int, offs: int) -> dict[str, float]:
+        return {
+            f"{KEY_TEMP_VALUE}{idx}": (
+                int.from_bytes(
+                    data[offs + idx * 2 : offs + (idx + 1) * 2],
+                    byteorder="big",
+                    signed=False,
+                )
+                - 2731.5
+            )
+            / 10
+            for idx in range(sensors)
+            if int.from_bytes(
+                data[offs + idx * 2 : offs + (idx + 1) * 2],
+                byteorder="big",
+                signed=False,
+            )
         }
 
     @staticmethod
@@ -196,19 +214,34 @@ class BMS(BaseBMS):
                 )
             )
             / 1000
-            for idx in range(data[9])
+            for idx in range(data[BMS._CELL_POS])
         }
 
     async def _async_update(self) -> BMSsample:
         """Update battery status information."""
 
         for cmd, data in BMS._CMDS:
-            await self._client.write_gatt_char(BMS.uuid_tx(), data=BMS._cmd(cmd, data=bytearray(data)))
+            await self._client.write_gatt_char(
+                BMS.uuid_tx(), data=BMS._cmd(cmd, data=bytearray(data))
+            )
             await asyncio.wait_for(self._wait_event(), timeout=BAT_TIMEOUT)
 
-        result: BMSsample = BMS._decode_data(self._data_final)
-        result |= BMS._cell_voltages(self._data_final[0x61])
+        result: BMSsample = {KEY_CELL_COUNT: int(self._data_final[0x61][BMS._CELL_POS])}
+        result[KEY_TEMP_SENS] = int(
+            self._data_final[0x61][BMS._CELL_POS + int(result[KEY_CELL_COUNT]) * 2 + 1]
+        )
+        result[KEY_PACK_COUNT] = min(int(self._data_final[0x51][42]), BMS._MAX_SUBS)
 
+        result |= BMS._cell_voltages(self._data_final[0x61])
+        result |= BMS._temp_sensors(
+            self._data_final[0x61],
+            int(result[KEY_TEMP_SENS]),
+            BMS._CELL_POS + int(result[KEY_CELL_COUNT]) * 2 + 2,
+        )
+        result |= BMS._decode_data(
+            self._data_final,
+            BMS._CELL_POS + int(result[KEY_CELL_COUNT] + result[KEY_TEMP_SENS]) * 2 + 2,
+        )
         self._data_final.clear()
 
         return result
