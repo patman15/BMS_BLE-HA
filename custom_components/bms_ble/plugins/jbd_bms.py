4463450e4f1493641759eda8e7e4facb38554e34
patman15 <14628713+patman15@users.noreply.github.com>
2025-02-11 21:32:17 +0100

    fix advertisement, adjust pattern

--- custom_components/bms_ble/plugins/jbd_bms.py
+++ custom_components/bms_ble/plugins/jbd_bms.py
@@ -57,19 +57,23 @@ class BMS(BaseBMS):
                 "service_uuid": BMS.uuid_services()[0],
                 "connectable": True,
             }
-            for pattern in ["SP0?S*", "SP1?S*", "SP2?S*", "GJ-*", "SX1*"]
+            for pattern in [
+                "SP0?S*",
+                "SP1?S*",
+                "SP2?S*",
+                "GJ-*",  # accurat batteries
+                "SX1*",  # Supervolt v3
+                "121?0*",  # Eleksol
+                "12200*",
+                "12300*",
+            ]
         ] + [
-            { # ECO-WORTHY LiFePO4
+            {  # ECO-WORTHY LiFePO4
                 "service_uuid": BMS.uuid_services()[0],
                 "manufacturer_id": manufacturer_id,
                 "connectable": True,
             }
             for manufacturer_id in [0x1852, 0x2298]
-        ] + [
-            { # Eleksol
-                "service_uuid": "00010203-0405-0607-0809-0a0b0c0d1912",
-                "connectable": True,
-            }
         ]
 
     @staticmethod
--- tests/advertisement_data.py
+++ tests/advertisement_data.py
@@ -268,15 +268,11 @@ ADVERTISEMENTS: Final[list] = [
         "jbd_bms",
     ),
     (  # source LOG (https://github.com/patman15/BMS_BLE-HA/issues/173)
-        generate_advertisement_data(  # Eleksol
+        generate_advertisement_data(  # Eleksol 12V300AH
             local_name="12300DE00013",
-            manufacturer_data={44580: b"\x27\x37\xc2\xa5"},  # MAC address
+            manufacturer_data={44580: b"\x27\x37\xc2\xa5"},  # MAC address, wrong
             service_uuids=[
-                "00001800-0000-1000-8000-00805f9b34fb",
-                "00001801-0000-1000-8000-00805f9b34fb",
-                "0000180a-0000-1000-8000-00805f9b34fb",
                 "0000ff00-0000-1000-8000-00805f9b34fb",
-                "00010203-0405-0607-0809-0a0b0c0d1912",
             ],
             rssi=-60,
         ),
