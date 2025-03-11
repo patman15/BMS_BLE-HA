21fcb4b14f457ab3a1764585250df8ba90bae4c0
patman15 <14628713+patman15@users.noreply.github.com>
2025-03-11 19:14:07 +0100

    add unique BMS matcher test

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -1,11 +1,16 @@
 """Test the BLE Battery Management System base class functions."""
 
+from collections.abc import Callable
+import importlib
 from types import ModuleType
+
+from home_assistant_bluetooth import BluetoothServiceInfoBleak
 
 from custom_components.bms_ble.const import BMS_TYPES
 from custom_components.bms_ble.plugins.basebms import BaseBMS
 
 from .advertisement_data import ADVERTISEMENTS
+from .bluetooth import generate_ble_device
 
 
 def test_device_info(plugin_fixture: ModuleType) -> None:
@@ -31,3 +36,33 @@ def test_advertisements_complete() -> None:
     assert (
         not bms_tocheck
     ), f"{len(bms_tocheck)} missing BMS type advertisements: {bms_tocheck}"
+
+
+def test_advertisements_unique() -> None:
+    """Check that each advertisement only matches one, the right BMS."""
+    fct_bms_supported: list[tuple[str, Callable[[BluetoothServiceInfoBleak], bool]]] = [
+        (
+            bms_type,
+            importlib.import_module(
+                f"custom_components.bms_ble.plugins.{bms_type}",
+                package=__name__[: __name__.rfind(".")],
+            ).BMS.supported,
+        )
+        for bms_type in BMS_TYPES
+    ]
+
+    for adv, bms_real in ADVERTISEMENTS:
+        for bms_test, fct_supported in fct_bms_supported:
+            supported: bool = fct_supported(
+                BluetoothServiceInfoBleak.from_scan(
+                    device=generate_ble_device(
+                        address="cc:cc:cc:cc:cc:cc",
+                        name="MockBLEDevice",
+                    ),
+                    advertisement_data=adv,
+                    source="test_advertisement_data",
+                    monotonic_time=0.0,
+                    connectable=True,
+                )
+            )
+            assert supported == (bms_real == bms_test), f"{adv} incorrectly matches {bms_test}!"
