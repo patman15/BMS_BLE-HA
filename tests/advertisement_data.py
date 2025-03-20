bbeef6933316fe32cfa92d7a61d6047365621cd6
Patrick <14628713+patman15@users.noreply.github.com>
2025-03-20 16:15:37 +0100

    Add JBD AP BMS type (#229)

--- tests/advertisement_data.py
+++ tests/advertisement_data.py
@@ -445,4 +445,15 @@ ADVERTISEMENTS: Final[list[tuple[AdvertisementData, str]]] = [
         ),
         "roypow_bms",
     ),
+    (  # source advmon (https://github.com/patman15/BMS_BLE-HA/issues/226)
+        generate_advertisement_data(  # A4:C1:37:42:3E:D9
+            local_name="AP21S002-l21s",  # not 100% sure
+            rssi=-84,
+            manufacturer_data={16089: "4237c1a4"},  # MAC address, wrong
+            service_uuids=[
+                "0000ff00-0000-1000-8000-00805f9b34fb",
+            ],
+        ),
+        "jbd_bms",
+    ),
 ]
