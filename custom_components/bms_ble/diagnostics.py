503cafed8fd2422f6eda953782e83c70c0954739
Patrick <14628713+patman15@users.noreply.github.com>
2025-04-05 21:29:38 +0200

    Add adapter to diagnostic data (#251)
    * add adapter to diagnostic data
    * complete test coverage

--- custom_components/bms_ble/diagnostics.py
+++ custom_components/bms_ble/diagnostics.py
@@ -3,10 +3,11 @@
 from typing import Any, Final
 
 from homeassistant.components.bluetooth import async_last_service_info
+from homeassistant.components.bluetooth.const import DOMAIN as BT_DOMAIN
 from homeassistant.components.diagnostics import async_redact_data
 from homeassistant.const import ATTR_AREA_ID, ATTR_ID
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.device_registry import DeviceEntry
+from homeassistant.helpers import device_registry as dr
 
 from . import BTBmsConfigEntry
 from .const import ATTR_LQ, ATTR_RSSI
@@ -16,18 +17,29 @@ TO_REDACT: frozenset[str] = frozenset({ATTR_ID, ATTR_AREA_ID})
 
 
 async def async_get_device_diagnostics(
-    hass: HomeAssistant, entry: BTBmsConfigEntry, device: DeviceEntry
+    hass: HomeAssistant, entry: BTBmsConfigEntry, device: dr.DeviceEntry
 ) -> dict[str, Any]:
     """Return diagnostics for a BMS device."""
+    adapter_info: str = "unavailable"
     coord: Final[BTBmsCoordinator] = entry.runtime_data
     mac: str = next(
         (id_value for domain, id_value in device.identifiers if domain == "bms_ble"), ""
     )
+    if (adv_data := async_last_service_info(hass, address=mac, connectable=True)) and (
+        adapter := dr.async_get(hass).async_get_device(
+            connections={(BT_DOMAIN, adv_data.source)}
+        )
+    ):
+        adapter_info = (
+            f"{adapter.name}, {adapter.manufacturer} ({adapter.model}/"
+            f"{adapter.model_id}): {adapter.hw_version}, {adapter.sw_version}"
+        )
 
     return {
         "entry_data": async_redact_data(entry.data, TO_REDACT),
         "device_data": async_redact_data(device.dict_repr, TO_REDACT),
-        "adv_data": async_last_service_info(hass, address=mac, connectable=True),
+        "adapter_data": adapter_info,
+        "adv_data": adv_data,
         "bms_data": entry.runtime_data.data,
         "bt_data": {ATTR_RSSI: coord.rssi, ATTR_LQ: coord.link_quality},
         "update_data": {
