95d927db45aa68556e98a121695859031ad089b8
patman15 <14628713+patman15@users.noreply.github.com>
2025-04-12 16:34:22 +0200

    basic decoding

--- custom_components/bms_ble/plugins/cbtpwr_vb_bms.py
+++ custom_components/bms_ble/plugins/cbtpwr_vb_bms.py
@@ -9,19 +9,22 @@ from bleak.backends.device import BLEDevice
 from bleak.uuids import normalize_uuid_str
 
 from custom_components.bms_ble.const import (
-    # ATTR_BATTERY_CHARGING,
+    ATTR_BATTERY_CHARGING,
     ATTR_BATTERY_LEVEL,
-    # ATTR_CURRENT,
+    ATTR_CURRENT,
     # ATTR_CYCLE_CAP,
     # ATTR_CYCLE_CHRG,
     # ATTR_CYCLES,
     ATTR_DELTA_VOLTAGE,
-    # ATTR_POWER,
-    # ATTR_RUNTIME,
-    # ATTR_TEMPERATURE,
-    # ATTR_VOLTAGE,
-    KEY_CELL_VOLTAGE,
     # KEY_PROBLEM,
+    ATTR_POWER,
+    ATTR_RUNTIME,
+    ATTR_TEMPERATURE,
+    ATTR_VOLTAGE,
+    KEY_CELL_COUNT,
+    KEY_CELL_VOLTAGE,
+    KEY_TEMP_SENS,
+    KEY_TEMP_VALUE,
 )
 
 from .basebms import BaseBMS, BMSsample  # , crc_sum
@@ -32,15 +35,15 @@ class BMS(BaseBMS):
 
     _HEAD: Final[bytes] = b"\x7e"
     _TAIL: Final[bytes] = b"\x0d"
-    _MIN_LEN: Final[int] = 10  # FIXME! define correctly
+    _LEN_POS: Final[int] = 9
+    _MIN_LEN: Final[int] = _LEN_POS + 3 + len(_HEAD) + len(_TAIL) + 4
     _MAX_LEN: Final[int] = 255
+    _CELL_POS: Final[int] = 6
 
-    _FIELDS: Final[list[tuple[str, int, int, Callable[[int], int | float]]]] = [
-        # (ATTR_VOLTAGE, 28, 4, lambda x: float(x) / 1000),
-        # (ATTR_CURRENT, 32, 4, lambda x: float(x) / 1000),
-        # (ATTR_TEMPERATURE, 4, 2, lambda x: x),
-        (ATTR_BATTERY_LEVEL, 8, 2, lambda x: x),
-        # (KEY_DESIGN_CAP, 0x15, 4, 2, lambda x: x),
+    _FIELDS: Final[list[tuple[str, int, int, bool, Callable[[int], int | float]]]] = [
+        (ATTR_VOLTAGE, 2, 2, False, lambda x: float(x) / 10),
+        (ATTR_CURRENT, 0, 2, True, lambda x: float(x) / 10),
+        (ATTR_BATTERY_LEVEL, 4, 2, False, lambda x: x),
         # (ATTR_CYCLES, 40, 2, lambda x: x),
         # (KEY_PROBLEM, 69, 4, lambda x: x),
     ]
@@ -48,6 +51,7 @@ class BMS(BaseBMS):
     def __init__(self, ble_device: BLEDevice, reconnect: bool = False) -> None:
         """Initialize BMS."""
         super().__init__(__name__, ble_device, reconnect)
+        self._exp_len: int = 0
 
     @staticmethod
     def matcher_dict_list() -> list[dict]:
@@ -63,7 +67,7 @@ class BMS(BaseBMS):
     @staticmethod
     def device_info() -> dict[str, str]:
         """Return device information for the battery management system."""
-        return {"manufacturer": "CBT Power", "model": "VB series"}
+        return {"manufacturer": "Seplos", "model": "VB series"}
 
     @staticmethod
     def uuid_services() -> list[str]:
@@ -86,7 +90,13 @@ class BMS(BaseBMS):
     @staticmethod
     def _calc_values() -> frozenset[str]:
         return frozenset(
-            {ATTR_DELTA_VOLTAGE}
+            {
+                ATTR_BATTERY_CHARGING,
+                ATTR_DELTA_VOLTAGE,
+                ATTR_TEMPERATURE,
+                ATTR_POWER,
+                ATTR_RUNTIME,
+            }
         )  # calculate further values from BMS provided set ones
 
     def _notification_handler(
@@ -94,7 +104,10 @@ class BMS(BaseBMS):
     ) -> None:
         """Handle the RX characteristics notify event (new data arrives)."""
 
-        if data.startswith(BMS._HEAD):
+        if len(data) > BMS._LEN_POS + 4 and data.startswith(BMS._HEAD):
+            self._exp_len = (
+                int(data[BMS._LEN_POS : BMS._LEN_POS + 4], 16) & 0x0FFF
+            )  # first byte unclear
             self._data = bytearray()
 
         self._data += data
@@ -102,14 +115,11 @@ class BMS(BaseBMS):
             "RX BLE data (%s): %s", "start" if data == self._data else "cnt.", data
         )
 
-        if not data.endswith(BMS._TAIL):
-            if len(self._data) >= BMS._MAX_LEN:
-                self._data.clear()
+        if len(self._data) < self._exp_len + BMS._MIN_LEN:
             return
 
-        # verify that data is long enough
-        if len(data) % 2 or len(data) < BMS._MIN_LEN:
-            self._log.debug("incorrect frame length (%i): %s", len(data), data)
+        if not self._data.endswith(BMS._TAIL):
+            self._log.debug("incorrect EOF: %s", data)
             self._data.clear()
             return
 
@@ -118,33 +128,57 @@ class BMS(BaseBMS):
             self._data.clear()
             return
 
-        # if (crc := crc_sum(data[5:-5], 2)) != int.from_bytes(
-        #     data[-4:-2], byteorder="little", signed=False
-        # ):
-        #     self._log.debug(
-        #         "invalid checksum 0x%X != 0x%X",
-        #         crc,
-        #         int.from_bytes(data[-4:-2], byteorder="little", signed=False),
-        #     )
-        #     return
+        if (crc := BMS._crc(data[1:-5])) != int(data[-5:-1], 16):
+            self._log.debug("invalid checksum 0x%X != 0x%X", crc, int(data[-5:-1], 16))
+            return
 
-        self._data = data[1:-1]
+        self._data = bytearray(
+            bytes.fromhex(self._data.strip(BMS._HEAD + BMS._TAIL).decode())
+        )
         self._data_event.set()
 
     @staticmethod
-    def _cell_voltages(data: bytearray) -> dict[str, float]:
+    def _crc(data: bytearray) -> int:
+        return ((sum(data) & 0xFFFF) ^ 0xFFFF) + 1
+
+    @staticmethod
+    def _cell_voltages(data: bytearray, cells: int) -> dict[str, float]:
         """Return cell voltages from status message."""
         return {
-            f"{KEY_CELL_VOLTAGE}{idx}": int(data[14 + 4 * idx : 18 + 4 * idx], 16)
+            f"{KEY_CELL_VOLTAGE}{idx}": float(
+                int.from_bytes(
+                    data[BMS._CELL_POS + 1 + idx * 2 : BMS._CELL_POS + idx * 2 + 3],
+                    byteorder="big",
+                    signed=False,
+                )
+            )
             / 1000
-            for idx in range(4)
+            for idx in range(cells)
         }
 
     @staticmethod
-    def _decode_data(frame: bytearray) -> BMSsample:
+    def _temp_sensors(data: bytearray, sensors: int, offs: int) -> dict[str, float]:
         return {
-            key: func(int(frame[idx : idx + size], 16))
-            for key, idx, size, func in BMS._FIELDS
+            f"{KEY_TEMP_VALUE}{idx}": (value) / 10
+            for idx in range(sensors)
+            if (
+                value := int.from_bytes(
+                    data[offs + idx * 2 : offs + (idx + 1) * 2],
+                    byteorder="big",
+                    signed=True,
+                )
+            )
+        }
+
+    @staticmethod
+    def _decode_data(data: bytearray, offs: int) -> BMSsample:
+        return {
+            key: func(
+                int.from_bytes(
+                    data[idx + offs : idx + offs + size], byteorder="big", signed=sign
+                )
+            )
+            for key, idx, size, sign, func in BMS._FIELDS
         }
 
     # @staticmethod
@@ -159,8 +193,29 @@ class BMS(BaseBMS):
 
     async def _async_update(self) -> BMSsample:
         """Update battery status information."""
-        await self._await_reply(
-            b"\x7e\x31\x31\x30\x31\x34\x36\x34\x32\x45\x30\x30\x32\x30\x31\x46\x44\x33\x35\x0d"
-        )  # request basic battery info
 
-        return BMS._decode_data(self._data) | BMS._cell_voltages(self._data)
+        await self._await_reply(
+            b"~11014642E00201FD35\r",
+        )
+
+        result: BMSsample = {KEY_CELL_COUNT: int(self._data[BMS._CELL_POS])}
+        temp_pos: Final[int] = (
+            BMS._CELL_POS + int(result.get(KEY_CELL_COUNT, 0)) * 2 + 1
+        )
+        result |= (
+            {KEY_TEMP_SENS: int(self._data[temp_pos])}
+            | BMS._cell_voltages(self._data, int(result.get(KEY_CELL_COUNT, 0)))
+            | BMS._temp_sensors(
+                self._data, int(result.get(KEY_TEMP_SENS, 0)), temp_pos + 1
+            )
+        )
+
+        # await self._await_reply(
+        #     b"~11014681A00601A101FC5F\r",
+        # )
+
+        result |= BMS._decode_data(
+            self._data, temp_pos + 2 * int(result.get(KEY_TEMP_SENS, 0)) + 1
+        )
+
+        return result
