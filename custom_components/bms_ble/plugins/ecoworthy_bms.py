e7cb91a2a300ce35137aad5409bc79bec827c32c
Patrick <14628713+patman15@users.noreply.github.com>
2025-06-02 18:52:39 +0200

    Fixed current factor for ECO-WORTHY V2 protocol (#348)
    * fixed current factor for V2 protocol
    * remove unnecessary float()

--- custom_components/bms_ble/plugins/ecoworthy_bms.py
+++ custom_components/bms_ble/plugins/ecoworthy_bms.py
@@ -17,17 +17,26 @@ class BMS(BaseBMS):
     _HEAD: Final[tuple] = (b"\xa1", b"\xa2")
     _CELL_POS: Final[int] = 14
     _TEMP_POS: Final[int] = 80
-    _FIELDS: Final[list[tuple[BMSvalue, int, int, int, bool, Callable[[int], Any]]]] = [
+    _FIELDS_V1: Final[
+        list[tuple[BMSvalue, int, int, int, bool, Callable[[int], Any]]]
+    ] = [
         ("battery_level", 0xA1, 16, 2, False, lambda x: x),
-        ("voltage", 0xA1, 20, 2, False, lambda x: float(x / 100)),
-        ("current", 0xA1, 22, 2, True, lambda x: float(x / 100)),
+        ("voltage", 0xA1, 20, 2, False, lambda x: x / 100),
+        ("current", 0xA1, 22, 2, True, lambda x: x / 100),
         ("problem_code", 0xA1, 51, 2, False, lambda x: x),
-        ("design_capacity", 0xA1, 26, 2, False, lambda x: float(x / 100)),
+        ("design_capacity", 0xA1, 26, 2, False, lambda x: x // 100),
         ("cell_count", 0xA2, _CELL_POS, 2, False, lambda x: x),
         ("temp_sensors", 0xA2, _TEMP_POS, 2, False, lambda x: x),
         # ("cycles", 0xA1, 8, 2, False, lambda x: x),
     ]
-    _CMDS: Final[set[int]] = set({field[1] for field in _FIELDS})
+    _FIELDS_V2: Final[
+        list[tuple[BMSvalue, int, int, int, bool, Callable[[int], Any]]]
+    ] = [
+        (*field[:-1], (lambda x: x / 10) if field[0] == "current" else field[-1])
+        for field in _FIELDS_V1
+    ]
+
+    _CMDS: Final[set[int]] = set({field[1] for field in _FIELDS_V1})
 
     def __init__(self, ble_device: BLEDevice, reconnect: bool = False) -> None:
         """Initialize BMS."""
@@ -117,7 +126,9 @@ class BMS(BaseBMS):
     @staticmethod
     def _decode_data(data: dict[int, bytearray]) -> BMSsample:
         result: BMSsample = {}
-        for key, cmd, idx, size, sign, func in BMS._FIELDS:
+        for key, cmd, idx, size, sign, func in (
+            BMS._FIELDS_V1 if data[0xA1].startswith(BMS._HEAD) else BMS._FIELDS_V2
+        ):
             result[key] = func(
                 int.from_bytes(
                     data[cmd][idx : idx + size], byteorder="big", signed=sign
