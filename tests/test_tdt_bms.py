d91692be58853b098586656444785201061c77c1
patman15 <14628713+patman15@users.noreply.github.com>
2025-06-06 16:55:01 +0200

    fix timeout patch for TDT  tests

--- tests/test_tdt_bms.py
+++ tests/test_tdt_bms.py
@@ -231,7 +231,7 @@ async def test_update_0x1e_head(monkeypatch, patch_bms_timeout, patch_bleak_clie
 
     monkeypatch.setattr(MockTDTBleakClient, "HEAD_CMD", 0x1E)
     monkeypatch.setattr(MockTDTBleakClient, "RESP", resp_0x1e)
-    patch_bms_timeout("tdt_bms")
+    patch_bms_timeout()
     patch_bleak_client(MockTDTBleakClient)
 
     bms = BMS(
