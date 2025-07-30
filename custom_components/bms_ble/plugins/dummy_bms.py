a43dc6db8e23ba186dbf4b647ff0a75c6daf0c2b
patman15 <14628713+patman15@users.noreply.github.com>
2025-07-30 22:09:19 +0200

    Update dummy_bms.py

--- custom_components/bms_ble/plugins/dummy_bms.py
+++ custom_components/bms_ble/plugins/dummy_bms.py
@@ -21,27 +21,27 @@ class BMS(BaseBMS):
     @staticmethod
     def matcher_dict_list() -> list[AdvertisementPattern]:
         """Provide BluetoothMatcher definition."""
-        return [{"local_name": "dummy", "connectable": True}]
+        return [{"local_name": "dummy", "connectable": True}]  # TODO
 
     @staticmethod
     def device_info() -> dict[str, str]:
         """Return device information for the battery management system."""
-        return {"manufacturer": "Dummy Manufacturer", "model": "dummy model"}
+        return {"manufacturer": "Dummy Manufacturer", "model": "dummy model"}  # TODO
 
     @staticmethod
     def uuid_services() -> list[str]:
         """Return list of 128-bit UUIDs of services required by BMS."""
-        return [normalize_uuid_str("0000")]  # change service UUID here!
+        return [normalize_uuid_str("0000")]  # TODO: change service UUID here!
 
     @staticmethod
     def uuid_rx() -> str:
         """Return 16-bit UUID of characteristic that provides notification/read property."""
-        return "#changeme"
+        return "0000"  # TODO: change RX characteristic UUID here!
 
     @staticmethod
     def uuid_tx() -> str:
         """Return 16-bit UUID of characteristic that provides write property."""
-        return "#changeme"
+        return "0000"  # TODO: change TX characteristic UUID here!
 
     @staticmethod
     def _calc_values() -> frozenset[BMSvalue]:
@@ -53,11 +53,11 @@ class BMS(BaseBMS):
         self, _sender: BleakGATTCharacteristic, data: bytearray
     ) -> None:
         """Handle the RX characteristics notify event (new data arrives)."""
-        self._log.debug("RX BLE data: %s", data)
+        # self._log.debug("RX BLE data: %s", data)
 
         # *******************************************************
-        # # Do things like checking correctness of frame here and
-        # # store it into a instance variable, e.g. self._data
+        # # TODO: Do things like checking correctness of frame here
+        # # and store it into a instance variable, e.g. self._data
         # # Below are some examples of how to do it
         # # Have a look at the BMS base class for function to use,
         # # take a look at other implementations for more  details
@@ -71,18 +71,18 @@ class BMS(BaseBMS):
         #     self._log.debug("invalid checksum 0x%X != 0x%X", self._data[-1], crc)
         #     return
 
-        self._data = data.copy()
-        self._data_event.set()
+        # self._data = data.copy()
+        # self._data_event.set()
 
     async def _async_update(self) -> BMSsample:
         """Update battery status information."""
         self._log.debug("replace with command to UUID %s", BMS.uuid_tx())
-        # await self._await_reply(b"<some_command>")
-        # #
-        # # parse data from self._data here
+        await self._await_reply(b"<some_command>")
+
+        # # TODO: parse data from self._data here
 
         return {
             "voltage": 12,
             "current": 1.5,
             "temperature": 27.182,
-        }  # fixed values, replace parsed data
+        }  # TODO: fixed values, replace parsed data
