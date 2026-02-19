f967a04e1a570fdf3dfffa2f117cc22ff1bf7e78
Patrick <14628713+patman15@users.noreply.github.com>
2026-02-19 22:15:02 +0100

    Update const.py

--- custom_components/bms_ble/const.py
+++ custom_components/bms_ble/const.py
@@ -5,7 +5,7 @@ from typing import Final
 
 DOMAIN: Final[str] = "bms_ble"
 LOGGER: Final[logging.Logger] = logging.getLogger(__package__)
-LOW_RSSI: Final = -75  # dBm considered low signal strength
+LOW_RSSI: Final[int] = -75  # dBm considered low signal strength
 UPDATE_INTERVAL: Final[int] = 30  # [s]
 
 # attributes (do not change)
